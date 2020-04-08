95471c6bc34d3848dae79ab71c59324e5c45b8b6
RomanPolozhii <roman.polozhii@gmail.com>
2020-04-08 18:30:31 +0300

    added author section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function(){
-    var mixer = mixitup('.products__inner-box');
+$(function () {
+  $(".rate-star").rateYo({
+    rating: 3.6,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $(".product-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
+  var mixer = mixitup(".products__inner-box");
 });
